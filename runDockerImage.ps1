4ae76120c93d2d11f4f40da8a29770eccec3c69e
Kai Walter <kai.walter@zeiss.com>
2018-04-04 08:32:39 +0000

    updated dependencies

---
+++ runDockerImage.ps1
@@ -0,0 +1,26 @@
+param(
+    $imageName = 'functions-container'
+)
+
+# Run the container
+$choice = Read-Host "interactive or detached [i/D]"
+if ($choice.ToLower() -eq "i") {
+    docker run -p 80:80 -it --rm --env-file .\hostvariables.env $imageName
+}
+else {
+    $containerId = (docker run -p 80:80 -d --name $imageName --env-file .\hostvariables.env $imageName)
+    Write-Host "started in container " $containerId
+    $container = (docker inspect $containerId) | ConvertFrom-Json
+    $ipAddress = $container.NetworkSettings.Networks.nat.IPAddress
+    
+    $delaySeconds = 10
+    Write-Host "testing the container in $($delaySeconds)s..."
+    Start-Sleep -Seconds $delaySeconds
+    $url = "http://" + $ipAddress + ":80/api/HttpTrigger?name=TEST" + (Get-Date -Format s)
+    [Diagnostics.Process]::Start($url)
+}
+
+
+
+
+
