54b50514591fc46f42d4f9a9c2d970926b629305
Kai Walter <kai.walter@zeiss.com>
2018-04-04 18:43:54 +0000

    add push

--- runDockerImage.ps1
+++ runDockerImage.ps1
@@ -8,16 +8,18 @@ if ($choice.ToLower() -eq "i") {
     docker run -p 80:80 -it --rm --env-file .\hostvariables.env $imageName
 }
 else {
-    $containerId = (docker run -p 80:80 -d --name $imageName --env-file .\hostvariables.env $imageName)
-    Write-Host "started in container " $containerId
-    $container = (docker inspect $containerId) | ConvertFrom-Json
-    $ipAddress = $container.NetworkSettings.Networks.nat.IPAddress
-    
-    $delaySeconds = 10
-    Write-Host "testing the container in $($delaySeconds)s..."
-    Start-Sleep -Seconds $delaySeconds
-    $url = "http://" + $ipAddress + ":80/api/HttpTrigger?name=TEST" + (Get-Date -Format s)
-    [Diagnostics.Process]::Start($url)
+    $containerId = (docker run -p 80:80 -d --rm --name $imageName --env-file .\hostvariables.env $imageName)
+    if($containerId) {
+        Write-Host "started in container " $containerId
+        $container = (docker inspect $containerId) | ConvertFrom-Json
+        $ipAddress = $container.NetworkSettings.Networks.nat.IPAddress
+        
+        $delaySeconds = 10
+        Write-Host "testing the container in $($delaySeconds)s..."
+        Start-Sleep -Seconds $delaySeconds
+        $url = "http://" + $ipAddress + ":80/api/HttpTrigger?name=TEST" + (Get-Date -Format s)
+        [Diagnostics.Process]::Start($url)
+    }
 }
 
 
